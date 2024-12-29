xfce-mirror/xfce4-xkb-plugin

commit 0ae511004167510ba093ee5e8d465c6577abc050

build: Bump dependency versions and fix deprecation warnings

panel-plugin/xkb-dialog.c
@@ -123,9 +123,10 @@ xkb_dialog_configure_plugin (XfcePanelPlugin *plugin,
 
   xfce_panel_plugin_block_menu (plugin);
 
-  settings_dialog = xfce_titled_dialog_new_with_buttons (_("Keyboard Layouts"),
-                                                         NULL, 0, "gtk-close",
-                                                         GTK_RESPONSE_OK, NULL);
+  settings_dialog = xfce_titled_dialog_new_with_mixed_buttons (_("Keyboard Layouts"),
+                                                               NULL, 0, "gtk-close",
+                                                               "window-close-symbolic", _("_Close"), GTK_RESPONSE_OK,
+                                                               NULL);
   gtk_window_set_icon_name (GTK_WINDOW (settings_dialog), "org.xfce.panel.xkb");
 
   vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 18);

configure.ac
@@ -64,15 +64,15 @@ dnl ***********************************
 dnl *** Check for required packages ***
 dnl ***********************************
 XDT_CHECK_PACKAGE([GLIB], [glib-2.0], [2.50.0])
-XDT_CHECK_PACKAGE([GTK], [gtk+-3.0], [3.20.0])
-XDT_CHECK_PACKAGE([LIBXFCE4PANEL], [libxfce4panel-2.0], [4.12.0])
-XDT_CHECK_PACKAGE([LIBXFCE4UTIL], [libxfce4util-1.0], [4.12.0])
-XDT_CHECK_PACKAGE([LIBXFCE4UI], [libxfce4ui-2], [4.12.0])
-XDT_CHECK_PACKAGE([XFCONF], [libxfconf-0], [4.12.1])
+XDT_CHECK_PACKAGE([GTK], [gtk+-3.0], [3.22.0])
+XDT_CHECK_PACKAGE([LIBXFCE4PANEL], [libxfce4panel-2.0], [4.16.0])
+XDT_CHECK_PACKAGE([LIBXFCE4UTIL], [libxfce4util-1.0], [4.16.0])
+XDT_CHECK_PACKAGE([LIBXFCE4UI], [libxfce4ui-2], [4.16.0])
+XDT_CHECK_PACKAGE([XFCONF], [libxfconf-0], [4.16.0])
 XDT_CHECK_PACKAGE([LIBXKLAVIER], [libxklavier], [5.3])
 XDT_CHECK_PACKAGE([LIBRSVG], [librsvg-2.0], [2.40])
 XDT_CHECK_PACKAGE([LIBWNCK], [libwnck-3.0], [3.14])
-XDT_CHECK_PACKAGE([GARCON], [garcon-1], [0.4.0])
+XDT_CHECK_PACKAGE([GARCON], [garcon-1], [4.16.0])
 
 dnl **********************************
 dnl *** Optional libnotify Support ***

panel-plugin/xkb-plugin.c
@@ -568,13 +568,14 @@ xkb_plugin_configure_layout (GtkWidget *widget)
   if (item)
     {
       g_shell_parse_argv (garcon_menu_item_get_command (item), NULL, &argv, &error);
-      succeed = xfce_spawn_on_screen (gtk_widget_get_screen (widget),
-                                      garcon_menu_item_get_path (item),
-                                      argv, NULL, G_SPAWN_SEARCH_PATH,
-                                      garcon_menu_item_supports_startup_notification (item),
-                                      gtk_get_current_event_time (),
-                                      garcon_menu_item_get_icon_name (item),
-                                      &error);
+      succeed = xfce_spawn (gtk_widget_get_screen (widget),
+                            garcon_menu_item_get_path (item),
+                            argv, NULL, G_SPAWN_SEARCH_PATH,
+                            garcon_menu_item_supports_startup_notification (item),
+                            gtk_get_current_event_time (),
+                            garcon_menu_item_get_icon_name (item),
+                            TRUE,
+                            &error);
       g_strfreev (argv);
       garcon_menu_item_unref (item);
       g_assert (succeed);

commit 14dcab5dcde974542ee8623ac3a62feeebbf07ee

prefs-dialog: Remove unused stock icon name
Fixes: 0ae511004167510ba093ee5e8d465c6577abc050
Closes: #59

panel-plugin/xkb-dialog.c
@@ -123,8 +123,7 @@ xkb_dialog_configure_plugin (XfcePanelPlugin *plugin,
 
   xfce_panel_plugin_block_menu (plugin);
 
-  settings_dialog = xfce_titled_dialog_new_with_mixed_buttons (_("Keyboard Layouts"),
-                                                               NULL, 0, "gtk-close",
+  settings_dialog = xfce_titled_dialog_new_with_mixed_buttons (_("Keyboard Layouts"), NULL, 0,
                                                                "window-close-symbolic", _("_Close"), GTK_RESPONSE_OK,
                                                                NULL);
   gtk_window_set_icon_name (GTK_WINDOW (settings_dialog), "org.xfce.panel.xkb");
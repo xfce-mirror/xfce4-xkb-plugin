14dcab5dcde974542ee8623ac3a62feeebbf07ee
Gaël Bonithon <gael@xfce.org>
2024-12-29 19:05:31 +0100

    prefs-dialog: Remove unused stock icon name
    Fixes: 0ae511004167510ba093ee5e8d465c6577abc050
    Closes: #59

--- panel-plugin/xkb-dialog.c
+++ panel-plugin/xkb-dialog.c
@@ -123,8 +123,7 @@ xkb_dialog_configure_plugin (XfcePanelPlugin *plugin,
 
   xfce_panel_plugin_block_menu (plugin);
 
-  settings_dialog = xfce_titled_dialog_new_with_mixed_buttons (_("Keyboard Layouts"),
-                                                               NULL, 0, "gtk-close",
+  settings_dialog = xfce_titled_dialog_new_with_mixed_buttons (_("Keyboard Layouts"), NULL, 0,
                                                                "window-close-symbolic", _("_Close"), GTK_RESPONSE_OK,
                                                                NULL);
   gtk_window_set_icon_name (GTK_WINDOW (settings_dialog), "org.xfce.panel.xkb");
